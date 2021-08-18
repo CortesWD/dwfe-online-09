a26a65e08d036c0dc440496a13706b1a36ef6d64
CortesWD <cccp.designer@gmail.com>
2021-08-17 21:01:42 -0500

    sesion 37

---
+++ sesion 37/src/App.js
@@ -0,0 +1,57 @@
+/**
+ * Dependencies
+ */
+import React, { useState } from 'react';
+
+/**
+ * Styles
+ */
+import './App.css';
+
+function App() {
+  const [name, setName] = useState('');
+  const [disabled, setDisabled] = useState(false);
+
+  const handleName = (e) => {
+    const { value } = e;
+    // const { target: { name, value } } = e;
+    // const obj = {
+    //   [`${name}`]: value
+    // };
+
+    // console.log(obj)
+    setName(value);
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    setDisabled(true);
+
+    setTimeout(() => {
+      console.log('el nombre guardado es', name);
+      setDisabled(false);
+    }, 3000);
+  }
+
+  return (
+    <form className="app">
+      <input
+        disabled={disabled}
+        type="text"
+        name="calavera"
+        value={name}
+        onChange={handleName}
+      />
+      <button
+        disabled={disabled}
+        className="button"
+        type="submit"
+        onClick={handleSubmit}
+      >
+        Enviar
+      </button>
+    </form>
+  );
+}
+
+export default App;
