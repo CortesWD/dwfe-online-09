a7ed2955d7652ee804742c00a9c226da78346efe
CortesWD <cccp.designer@gmail.com>
2021-08-26 20:40:41 -0500

    dark mode

--- sesion 40/src/App.js
+++ sesion 40/src/App.js
@@ -1,25 +1,40 @@
 /**
  * Dependencies
  */
-import React from 'react';
+import React, { useState } from 'react';
 
 /**
  * Components
  */
+import Button from './components/button/Button';
+import LoginForm from './components/loginForm/LoginForm';
 
 /**
  * Styles
  */
 import './App.css';
-import LoginForm from './components/loginForm/LoginForm';
+
 
 /**
  * Others
  */
 
 function App() {
+  const [isDark, setIsDark] = useState(false);
 
-  return (<LoginForm />);
+  return (
+    <section className={`app ${isDark ? 'dark' : 'light'}`}>
+      <div className="container">
+        <Button
+          onClick={() => setIsDark(!isDark)}
+        >
+          MODO
+          {isDark ? ' LIGHT' : ' DARK'}
+        </Button>
+        <LoginForm />
+      </div>
+    </section>
+  );
 }
 
 export default App;
