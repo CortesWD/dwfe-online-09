743982892a3049284a1d389290ed02e144909599
CortesWD <cccp.designer@gmail.com>
2021-09-28 20:39:58 -0500

    sesion 49 part2

--- sesion 49/async.js
+++ sesion 49/async.js
@@ -1,1 +1,29 @@
 const app = document.getElementById('root');
+
+/**
+ * @param {String} term - string term to query
+ * @returns {Promise} fetch Promise
+ */
+ function request(term) {
+  return fetch(`https://api.giphy.com/v1/gifs/search?api_key=ErT4SajFmJKTRMjhTi1xTIYDMEm4DJED&q=${term}&limit=5`);
+};
+
+
+async function renderGifs() {
+  const response = await request('cat');
+  const json = await response.json();
+  const { data } = json;
+  const { status } = response;
+
+  if (status === 200) {
+    data.forEach(gif => {
+      const element = document.createElement('img');
+      element.src = gif.images.fixed_height.url;
+      app.appendChild(element);
+    }); 
+  } else {
+    app.innerText = 'ocurrio un error al traer gifs';
+  }
+}
+
+renderGifs();
